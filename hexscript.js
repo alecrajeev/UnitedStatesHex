e0716939f07a5257ad5336e7b87be86de8d57f22
Alec Rajeev <rajeeva2012@gmail.com>
2015-08-03 17:07:44 -0400

    Update hexscript.js
    This gets rid of the part of the file that builds the topoJSON file and moves it offline. Now this just imports a .json file and displays it.

--- hexscript.js
+++ hexscript.js
@@ -4,149 +4,92 @@ var width = 1500,
 
 // 90 x 60 (width x height)
 
-var topology = hexTopology(radius, width, height);
+d3.json("ushex.json", start);
 
-var projection = hexProjection(radius);
+function start(error, ushex) {
 
-var path = d3.geo.path()
-    .projection(projection);
+  if (error)
+      return console.warn(error);
 
-var svg = d3.select(".hexagonal").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  var projection = hexProjection(radius);
 
-console.log(topology);
-console.log(topology.objects.hexagons);
+  var path = d3.geo.path()
+      .projection(projection);
 
-var hexFeatures = topology.objects.states.geometries;
-var hexFeatures = topojson.feature(topology, topology.objects.states).features;
+  var svg = d3.select(".hexagonal").append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-var hexagons = svg.append("g")
-    .attr("class", "hexagon")
-  .selectAll("path")
-    .data(hexFeatures)
-  .enter().append("path")
-    .attr("d", function(d) { return path(topojson.feature(topology, d)); })
-    // .attr("class", function(d) { return d.fill ? "fill" : null; })
-    .attr("class", function(d) {
-      if (d.count % 2 == 0)
-        return "even";
-      else
-        return "odd";
-    })
-    .on("mousedown", mousedown)
-    .on("mousemove", mousemove)
-    .on("mouseup", mouseup);
+  var hexFeatures = topojson.feature(ushex, ushex.objects.states).features;
+  console.log(ushex);
 
-svg.append("path")
-    .datum(topojson.mesh(topology, topology.objects.states))
-    .attr("class", "mesh")
-    .attr("d", path);
+  var hexagons = svg.append("g")
+      .attr("class", "hexagon")
+    .selectAll("path")
+      .data(hexFeatures)
+    .enter().append("path")
+      .attr("d", path)
+      // .attr("class", function(d) { return d.fill ? "fill" : null; })
+      .attr("class", function(d) {
+        if (d.count % 2 == 0)
+          return "even";
+        else
+          return "odd";
+      })
+      .on("mousedown", mousedown)
+      .on("mousemove", mousemove)
+      .on("mouseup", mouseup);
 
-var border = svg.append("path")
-    .attr("class", "border")
-    .call(redraw);
+  var hexMesh = svg.append("path")
+      .datum(topojson.mesh(ushex, ushex.objects.states))
+      .attr("class", "hexMesh")
+      .attr("d", path);
 
-var mousing = 0;
+  var border = svg.append("path")
+      .attr("class", "border")
+      .call(redraw);
 
-window.topology = topology;
+  var mousing = 0;
 
-function mousedown(d) {
-  mousing = d.fill ? -1 : +1;
-  mousemove.apply(this, arguments);
-  d.cd = "Cali"
-  console.log(d.count);
-}
+  window.ushex = ushex;
 
-function mousemove(d) {
-  if (mousing) {
-    d3.select(this).classed("fill", d.fill = mousing > 0);
-    border.call(redraw);
+  function mousedown(d) {
+    mousing = d.fill ? -1 : +1;
+    mousemove.apply(this, arguments);
+    d.cd = "Cali"
+    console.log(d.count);
   }
-}
 
-function mouseup() {
-  mousemove.apply(this, arguments);
-  mousing = 0;
-}
-
-function redraw(border) {
-  // border.attr("d", path(topojson.mesh(topology, topology.objects.states, function(a, b) {return a.fill ^ b.fill; })));
-  border.attr("d", path(topojson.mesh(topology, topology.objects.states, function(a, b) {return a.state != b.state;  })));
-}
-
-function hexTopology(radius, width, height) {
-  var dx = radius * 2 * Math.sin(Math.PI / 3),
-      dy = radius * 1.5,
-      m = Math.ceil((height + radius) / dy) + 1,
-      n = Math.ceil(width / dx) + 1, // number across and down one level
-      geometries = [],
-      statesgeo = [],
-      arcs = [];
-
-  var hexCount = 0;
-
-  for (var j = -1; j <= m; ++j) {
-    for (var i = -1; i <= n; ++i) {
-      var y = j * 2, x = (i + (j & 1) / 2) * 2;
-      arcs.push([[x, y - 1], [1, 1]], [[x + 1, y], [0, 1]], [[x + 1, y + 1], [-1, 1]]);
+  function mousemove(d) {
+    if (mousing) {
+      d3.select(this).classed("fill", d.fill = mousing > 0);
+      border.call(redraw);
     }
   }
 
-  for (var j = 0, q = 3; j < m; ++j, q += 6) {
-    for (var i = 0; i < n; ++i, q += 3) {
-      geometries.push({
-        type: "Polygon",
-        arcs: [[q, q + 1, q + 2, ~(q + (n + 2 - (j & 1)) * 3), ~(q - 2), ~(q - (n + 2 + (j & 1)) * 3 + 2)]],
-        fill: Math.random() > i / n * 2,
-        count: hexCount++,
-      });
-
-      statesgeo.push({
-        type: "Polygon",
-        arcs: [[q, q + 1, q + 2, ~(q + (n + 2 - (j & 1)) * 3), ~(q - 2), ~(q - (n + 2 + (j & 1)) * 3 + 2)]],
-        fill: hexCount == 488 || hexCount == 489,
-        state: getState(hexCount),
-        count: hexCount,
-        cd: ""
-      });
-    }
+  function mouseup() {
+    mousemove.apply(this, arguments);
+    mousing = 0;
   }
 
-  function getState(i) {
-    if (i > 485 & i < 490) {
-      return "Iowa";
-    }
-    else {
-      if ( i > 390 & i < 395) {
-        return "Michigan";
+  function redraw(border) {
+    // border.attr("d", path(topojson.mesh(ushex, ushex.objects.states, function(a, b) {return a.fill ^ b.fill; })));
+    border.attr("d", path(topojson.mesh(ushex, ushex.objects.states, function(a, b) {return a.state != b.state;  })));
+  }
+
+  function hexProjection(radius) {
+    var dx = radius * 2 * Math.sin(Math.PI / 3),
+        dy = radius * 1.5;
+    return {
+      stream: function(stream) {
+        return {
+          point: function(x, y) { stream.point(x * dx / 2, (y - (2 - (y & 1)) / 3) * dy / 2); },
+          lineStart: function() { stream.lineStart(); },
+          lineEnd: function() { stream.lineEnd(); },
+          polygonStart: function() { stream.polygonStart(); },
+          polygonEnd: function() { stream.polygonEnd(); }
+        };
       }
-      else {
-        return "Oregon";
-      }
-    }
+    };
   }
-
-  return {
-    type: "Topology",
-    objects: {hexagons: {type: "GeometryCollection", geometries: geometries}, states: {type: "GeometryCollection", geometries: statesgeo}},
-    arcs: arcs,
-    transform: {translate: [0, 0], scale: [1, 1]}
-  };
-}
-
-function hexProjection(radius) {
-  var dx = radius * 2 * Math.sin(Math.PI / 3),
-      dy = radius * 1.5;
-  return {
-    stream: function(stream) {
-      return {
-        point: function(x, y) { stream.point(x * dx / 2, (y - (2 - (y & 1)) / 3) * dy / 2); },
-        lineStart: function() { stream.lineStart(); },
-        lineEnd: function() { stream.lineEnd(); },
-        polygonStart: function() { stream.polygonStart(); },
-        polygonEnd: function() { stream.polygonEnd(); }
-      };
-    }
-  };
 }
