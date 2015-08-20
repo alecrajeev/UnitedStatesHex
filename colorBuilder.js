04870ca9da02cbbe89770ee7e16c16ec7173bdfd
Alec Rajeev <rajeeva2012@gmail.com>
2015-08-20 00:48:17 -0400

    Changed where the color functions are located

---
+++ colorBuilder.js
@@ -0,0 +1,83 @@
+// this is the javascript file that has all the functions that regarding color
+
+var demoColor = d3.scale.threshold() // color scale for the demographic data
+	.range(['rgb(247,252,245)','rgb(229,245,224)','rgb(199,233,192)','rgb(161,217,155)','rgb(116,196,118)','rgb(65,171,93)','rgb(35,139,69)','rgb(0,109,44)','rgb(0,68,27)']);
+
+var presColor = d3.scale.threshold() // color scale for the presidential data
+	.range(['#AE000C','#BA3035','#C56365','#D09697','#DBC8C8','#C8C8D5','#9697BD','#6465A5','#32358E', 'rgb(1,10,121)'])
+	.domain([.18, .3, .35, .4, .45, .55, .6, .65, .7, .97]);;
+
+function buildColorRange(i) { // builds the color range
+	switch(i) {
+		case 0: // white
+			demoColor.range(['rgb(247,252,245)','rgb(229,245,224)','rgb(199,233,192)','rgb(161,217,155)','rgb(116,196,118)','rgb(65,171,93)','rgb(35,139,69)','rgb(0,109,44)','rgb(0,68,27)']);
+			break;
+		case 1: // black
+			demoColor.range(['rgb(252,251,253)','rgb(239,237,245)','rgb(218,218,235)','rgb(188,189,220)','rgb(158,154,200)','rgb(128,125,186)','rgb(106,81,163)','rgb(84,39,143)','rgb(63,0,125)']);
+			break;
+		case 2: // latino
+			demoColor.range(['rgb(255,245,235)','rgb(254,230,206)','rgb(253,208,162)','rgb(253,174,107)','rgb(253,141,60)','rgb(241,105,19)','rgb(217,72,1)','rgb(166,54,3)','rgb(127,39,4)']);
+			break;
+		case 3: // asian
+			demoColor.range(['rgb(255,245,240)','rgb(254,224,210)','rgb(252,187,161)','rgb(252,146,114)','rgb(251,106,74)','rgb(239,59,44)','rgb(203,24,29)','rgb(165,15,21)','rgb(103,0,13)']);
+			break;
+		case 4: // multiracial
+			demoColor.range(['rgb(247,251,255)','rgb(222,235,247)','rgb(198,219,239)','rgb(158,202,225)','rgb(107,174,214)','rgb(66,146,198)','rgb(33,113,181)','rgb(8,81,156)','rgb(8,48,107)']);
+			break;
+		default: // presidential, never actually used here
+			presColor.range(['#AE000C','#BA3035','#C56365','#D09697','#DBC8C8','#C8C8D5','#9697BD','#6465A5','#32358E', 'rgb(1,10,121)']);
+			break;
+	}
+}
+
+function buildColorDomain(extent) {
+ 	var colorDomain = [];
+	var j = 0;
+	for (i = extent[0]; i <= (extent[1]+.01); i += ((extent[1]+.01) - extent[0])/8.0) {
+		colorDomain[j++] = i;
+	}
+	return colorDomain;
+}
+
+function buildExtentData() { // builds the mininum and maximum value array, extent, for each dataset
+	extentData[0] = d3.extent(demoData, function(d) {return d.White;	});
+	extentData[1] = d3.extent(demoData, function(d) {return d.Black;	});
+	extentData[2] = d3.extent(demoData, function(d) {return d.Latino;	});
+	extentData[3] = d3.extent(demoData, function(d) {return d.Asian;	});
+	extentData[4] = d3.extent(demoData, function(d) {return d.Multiracial;	});
+	extentData[5] = d3.extent(presData, function(d) {return d.Obama2012;	});
+	extentData[6] = d3.extent(presData, function(d) {return d.Obama2008;	});
+}
+
+function showDemographics(i) { // fills in the hexagons with the correct color according to the scale
+	hexagons.style("fill", function(d) {
+			var districtID = d.properties.districtID;
+			if (districtID != -1) { // if it's an actual district and not part of the ocean
+				return demoColor(dataByDistrictID[districtID][i])
+			}
+		});
+
+	hexagons.style("stroke", function(d) { // the stroke has to be the same color as the hexagon for it to look correct
+			var districtID = d.properties.districtID;
+			if (districtID != -1) {
+				return demoColor(dataByDistrictID[districtID][i])
+			}
+		});
+}
+
+function showPresidential(i) {
+
+	hexagons.style("fill", function(d) {
+			var districtID = d.properties.districtID;
+			if (districtID != -1) {
+				return presColor(dataByDistrictID[districtID][i])
+			}
+		});
+
+	hexagons.style("stroke", function(d) {
+			var districtID = d.properties.districtID;
+			if (districtID != -1) {
+				return presColor(dataByDistrictID[districtID][i])
+			}
+		});
+}
--- hexscript.js
+++ hexscript.js
@@ -2,13 +2,6 @@ var width = 1250,
     height = 730,
     radius = 7;
 
-var demoColor = d3.scale.threshold()
-	.range(['rgb(247,252,245)','rgb(229,245,224)','rgb(199,233,192)','rgb(161,217,155)','rgb(116,196,118)','rgb(65,171,93)','rgb(35,139,69)','rgb(0,109,44)','rgb(0,68,27)']);
-
-var presColor = d3.scale.threshold()
-	.range(['#AE000C','#BA3035','#C56365','#D09697','#DBC8C8','#C8C8D5','#9697BD','#6465A5','#32358E', 'rgb(1,10,121)'])
-	.domain([.18, .3, .35, .4, .45, .55, .6, .65, .7, .97]);;
-
 var hexMesh, hexagons, demoData, presData;
 var dataByDistrictID = {};
 var specificDistrictID = -2;
@@ -71,9 +64,7 @@ function makeMyMap(error, ushex, ddata, presidentialData) {
 
 	var hexFeatures = topojson.feature(ushex, ushex.objects.states).features;
 
-	hexagons = svg.append("g")
-		.attr("class", "hexagon")
-		.selectAll("path")
+	hexagons = svg.append("g").attr("class", "hexagon").selectAll("hexagon")
 		.data(hexFeatures)
 		.enter()
 		.append("path")
@@ -81,14 +72,6 @@ function makeMyMap(error, ushex, ddata, presidentialData) {
 		.attr("class", function(d) {return d.properties.state;	})
 		.classed("state", true)
 		.on("mouseover", mouseover)
-		.on("mousedown", mousedown)
-		.on("mousemove", mousemove)
-		.on("mouseup", mouseup)
-
-	hexMesh = svg.append("path")
-		.datum(topojson.mesh(ushex, ushex.objects.states))
-		.attr("class", "noMesh")
-		.attr("d", path);
 
     var stateBorder = svg.append("path")
     	.attr("class", "stateBorder")
@@ -102,29 +85,10 @@ function makeMyMap(error, ushex, ddata, presidentialData) {
     	.attr("class", "specificBorder")
     	.call(drawSpecificDistrict);
 
- 	var mousing = 0;
-
  	function mouseover(d) {
   		specificDistrictID = d.properties.districtID;
  		specificDistrict.call(drawSpecificDistrict);
  		changeTooltip(d);	
- 	}
-
- 	function mousedown(d) {
- 		mousing = d.fill ? -1 : +1;
- 		mousemove.apply(this, arguments);
-		// console.log(d.properties.districtID + " " + d.properties.state + "-" + d.properties.district);
- 	}
-
- 	function mousemove(d) {
- 		if (mousing) {
- 			// d3.select(this).classed("fill", d.fill = mousing > 0);
- 		}
- 	}
-
- 	function mouseup() {
- 		mousemove.apply(this, arguments);
- 		mousing = 0;
  	}
 
  	function drawSpecificDistrict(border) {
@@ -165,7 +129,7 @@ function makeMyMap(error, ushex, ddata, presidentialData) {
  		return hex1.properties.state != hex2.properties.state;
  	}
 
-  	function hexProjection(radius) {
+  	function hexProjection(radius) { // comes from Mike Bostock's hexagon mesh source code
   	  	var dx = radius * 2 * Math.sin(Math.PI / 3),
   	  	    dy = radius * 1.5;
   	  	return {
@@ -180,48 +144,6 @@ function makeMyMap(error, ushex, ddata, presidentialData) {
   	  	  }
   	  	};
   	}
-}
-
-function buildColorDomain(extent) {
- 	var colorDomain = [];
-	var j = 0;
-	for (i = extent[0]; i <= (extent[1]+.01); i += ((extent[1]+.01) - extent[0])/8.0) {
-		colorDomain[j++] = i;
-	}
-	return colorDomain;
-}
-
-function buildColorRange(i) {
-	switch(i) {
-		case 0: // white
-			demoColor.range(['rgb(247,252,245)','rgb(229,245,224)','rgb(199,233,192)','rgb(161,217,155)','rgb(116,196,118)','rgb(65,171,93)','rgb(35,139,69)','rgb(0,109,44)','rgb(0,68,27)']);
-			break;
-		case 1: // black
-			demoColor.range(['rgb(252,251,253)','rgb(239,237,245)','rgb(218,218,235)','rgb(188,189,220)','rgb(158,154,200)','rgb(128,125,186)','rgb(106,81,163)','rgb(84,39,143)','rgb(63,0,125)']);
-			break;
-		case 2: // latino
-			demoColor.range(['rgb(255,245,235)','rgb(254,230,206)','rgb(253,208,162)','rgb(253,174,107)','rgb(253,141,60)','rgb(241,105,19)','rgb(217,72,1)','rgb(166,54,3)','rgb(127,39,4)']);
-			break;
-		case 3: // asian
-			demoColor.range(['rgb(255,245,240)','rgb(254,224,210)','rgb(252,187,161)','rgb(252,146,114)','rgb(251,106,74)','rgb(239,59,44)','rgb(203,24,29)','rgb(165,15,21)','rgb(103,0,13)']);
-			break;
-		case 4: // multiracial
-			demoColor.range(['rgb(247,251,255)','rgb(222,235,247)','rgb(198,219,239)','rgb(158,202,225)','rgb(107,174,214)','rgb(66,146,198)','rgb(33,113,181)','rgb(8,81,156)','rgb(8,48,107)']);
-			break;
-		default: // presidential, never actually used here
-			presColor.range(['#AE000C','#BA3035','#C56365','#D09697','#DBC8C8','#C8C8D5','#9697BD','#6465A5','#32358E', 'rgb(1,10,121)']);
-			break;
-	}
-}
-
-function buildExtentData() { // builds the mininum and maximum value array, extent, for each dataset
-	extentData[0] = d3.extent(demoData, function(d) {return d.White;	});
-	extentData[1] = d3.extent(demoData, function(d) {return d.Black;	});
-	extentData[2] = d3.extent(demoData, function(d) {return d.Latino;	});
-	extentData[3] = d3.extent(demoData, function(d) {return d.Asian;	});
-	extentData[4] = d3.extent(demoData, function(d) {return d.Multiracial;	});
-	extentData[5] = d3.extent(presData, function(d) {return d.Obama2012;	});
-	extentData[6] = d3.extent(presData, function(d) {return d.Obama2008;	});
 }
 
 function showStates() {
@@ -247,7 +169,7 @@ function showDataSet(i) {
 }
 
 function changeTooltip(d) {
-	if (d.properties.state != "Ocean") {
+	if (d.properties.state != "Ocean") { // if you're on a district
 		d3.select(".whichState").text(d.properties.state);
 		d3.select(".whichDistrict").text(d.properties.district);
 		for (i = 0; i < 7; i++) {
@@ -257,8 +179,8 @@ function changeTooltip(d) {
 			else
 				d3.select("." + classNameSplit[0] + classNameSplit[1] + ".Tooltip").text(dataSets[i] + ": " + d3.round(dataByDistrictID[d.properties.districtID][i]*100, 1) + "%");
 		}
-}
-	else {
+	}
+	else { // if you're not on a district
 		d3.select(".whichState").text("");
 		d3.select(".whichDistrict").text("");		
 		for (i = 0; i < 7; i++) {
@@ -271,39 +193,10 @@ function changeTooltip(d) {
 	}
 }
 
-function showDemographics(i) {
-	hexagons.style("fill", function(d) {
-			var districtID = d.properties.districtID;
-			if (districtID != -1) { // if it's an actual district and not part of the ocean
-				return demoColor(dataByDistrictID[districtID][i])
-			}
-		});
-
-	hexagons.style("stroke", function(d) {
-			var districtID = d.properties.districtID;
-			if (districtID != -1) {
-				return demoColor(dataByDistrictID[districtID][i])
-			}
-		});
+function hideSideBar() {
+	d3.select(".information").style("display", "none");
 }
 
-function showPresidential(i) {
-
-	hexagons.style("fill", function(d) {
-			var districtID = d.properties.districtID;
-			if (districtID != -1) {
-				return presColor(dataByDistrictID[districtID][i])
-			}
-		});
-
-	hexagons.style("stroke", function(d) {
-			var districtID = d.properties.districtID;
-			if (districtID != -1) {
-				return presColor(dataByDistrictID[districtID][i])
-			}
-		});
-}
-
-function showMesh() {
-	hexMesh.attr("class", "hexMesh");
+function showSidebar() {
+	d3.select(".information").style("display", "block");
 }
