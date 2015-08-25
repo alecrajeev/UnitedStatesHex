a1b483f1982fd03c2a09cac1e808d8f5b7bb58b0
Alec Rajeev <rajeeva2012@gmail.com>
2015-08-24 23:26:04 -0400

    Built a file that gets a list of the recent votes

--- voteBuilder.js
+++ voteBuilder.js
@@ -3,21 +3,42 @@
 // builds the json file to quickly list all the recent roll call votes
 
 var d3 = require("d3"),
-	fs = require("fs");
+	fs = require("fs"),
+	csv = require("fast-csv"),
+	request = require("request");
 
-var voteList = JSON.parse(fs.readFileSync("votes.json", "utf-8"));
+var voteList;
 var finalVoteList = [];
 
+// imports data from GovTrack
+request("https://www.govtrack.us/api/v2/vote?sort=-created&congress=114&chamber=house&limit=500", function (error, response, body) {
+  if (!error && response.statusCode == 200) {
 
-buildVotes(voteList);
+  	voteList = body;
 
+  	buildVotes(JSON.parse(voteList));
+  }
+})
+
+// makes the data from GovTrack more simple
 function buildVotes(voteList) {
+
+	finalVoteList.push(["id", "category", "number", "question"]);
 
 	voteList.objects.forEach(function(d) {
 		finalVoteList.push([d.id, d.category, d.number, d.question]);
+		
 	});
-}
 
-fs.writeFileSync("simplevotes.json", JSON.stringify(finalVoteList, null, 2));
+
+	// specific csv converter 
+	var writeableStream = fs.createWriteStream("votesExport.csv");
+
+	writeableStream.on("finish", function() {
+	console.log("done");
+	});
+
+	csv.write(finalVoteList, {headers: true}).pipe(writeableStream);
+}
 
 console.log("Completed Build");
