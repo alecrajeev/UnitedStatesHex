b1fe5b6353a0f587478db5f231a32b0c78b94206
Alec Rajeev <rajeeva2012@gmail.com>
2015-08-29 21:15:54 -0400

    Collected API data for first 4 bernie event list parts

--- sunlight.js
+++ sunlight.js
@@ -15,19 +15,22 @@ var districtList = {};
 
 var functionList = [];
 
-var writeableStream = fs.createWriteStream("districtListB.csv");
+var index = "4";
+
+var writeableStream = fs.createWriteStream("districtData/districtList" + index +".csv");
 
 writeableStream.on("finish", function() {
 	console.log("done");
 });
 
-var sunlightAPI = function(info,cb) {
+var sunlightAPI = function(info,cb){
 
 	var url = info[0];
 	var attendee_count = info[1]
 
     request.get(url, function(err,response,sunlightData){
 		if (err) {
+			console.log("oops");
 			cb(err);
 		} else {
 			
@@ -63,27 +66,7 @@ function getDistrictInfo(latitude, longitude, attendee_count, bernieID) {
 	return [url,attendee_count, bernieID];
 }
 
-var readDistrictList2 = function(info, cb) {
-
-	fs.readFile("districtListA.csv", "utf-8", function (err, districtListData) {
-		if (err)
-			console.error(err);
-
-		console.log(districtListData);
-
-		var districtListData = d3.csv.parse(districtListData);
-
-		districtList = [];
-
-		districtListData.forEach(function (d) {
-			d.districtID = +d.districtID;
-			districtList[d.districtID] = {statecd: d.statecd, attendee_count: +d.attendee_count};
-		})
-
-		// bernieReader2(districtList, info); // probably should eventually switch this to an async series or waterfall
-		cb(null, info);
-	});
-}
+var districtList = {};
 
 var readDistrictList = function() {
 	fs.readFile("districtListA.csv", "utf-8", function (err, districtListData) {
@@ -103,7 +86,7 @@ var readDistrictList = function() {
 
 var bernieReader = function(districtList) {
 
-	fs.readFile("bernieData/bernie0.json", "utf-8", function (err, data) {
+	fs.readFile("bernieData/bernie" + index + ".json", "utf-8", function (err, data) {
 
 		bernie = JSON.parse(data);
 
@@ -163,81 +146,4 @@ var bernieReader = function(districtList) {
 	}
 }
 
-var bernieReader2 = function(districtList, fileName) {
-
-	console.log(districtList);
-
-	fs.readFile(fileName, "utf-8", function (err, data) {
-
-		bernie = JSON.parse(data);
-
-		bernie = bernie.results;
-
-		var count = 0;
-
-		bernie.forEach(function (d) {
-			locationList.push([d.latitude, d.longitude, d.attendee_count, d.bernieID]);
-		});
-
-		locationList.forEach(function (d) {
-			functionList.push(getDistrictInfo(d[0],d[1],d[2],d[3]));
-		});
-
-		async.map(functionList, sunlightAPI, function (err, results) {
-		    if (err){
-		    	console.error(err);
-		       // either file1, file2 or file3 has raised an error, so you should not use results and handle the error
-		    } else {
-
-		    	// results is an array of the data loaded from the sunlight API
-		    	results.forEach(function (d) {
-
-		    		if (d != -1) {
-
-			    		var districtID = getdistrictID(d.statecd);
-
-			    		// currently just simply adding the attendee count for each district
-			    		// eventually will do something with dates
-			    		districtList[districtID].attendee_count += d.attendee_count;
-		    		}
-				});
-
-				// this is used to make it a format the csv writer can handle
-		    	var districtListConvert = [];
-		    	districtListConvert.push(["statecd", "districtID", "attendee_count"])
-
-		    	for (i = 0; i < 435; i++) {
-		    		districtListConvert.push([districtList[i].statecd, i, districtList[i].attendee_count]);
-		    	}
-
-		    	csv.write(districtListConvert, {headers: true}).pipe(writeableStream);
-			}
-		});
-				
-	});
-
-	function getdistrictID(statecd) {
-
-		for (i = 0; i < 435; i++) {
-			if (districtList[i].statecd === statecd) {
-				return i;
-			}
-		}
-		return -1;
-	}
-}
-
-var fileList = [];
-
-for (i = 0; i < 15; i++)
-	fileList.push("bernieData/bernie" + i.toString() + ".json");
-
-async.map(["bernieData/bernie0.json"], readDistrictList2, function (err, results) {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(results);
-	}
-});
-
-// readDistrictList();
+readDistrictList();
